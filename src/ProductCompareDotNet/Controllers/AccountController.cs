88b7642b32dd538c45118f351a8f4655a7e9d714
Andrew Cho <cho-andrew@hotmail.com>
2016-05-17 15:07:54 -0700

    show user's name rather than just profile

--- src/ProductCompareDotNet/Controllers/AccountController.cs
+++ src/ProductCompareDotNet/Controllers/AccountController.cs
@@ -6,6 +6,11 @@ using ProductCompareDotNet.ViewModels;
 using System.Linq;
 using System.Security.Principal;
 using System.Security.Claims;
+using System.Collections.Generic;
+using Microsoft.AspNet.Http;
+using System.IO;
+using Microsoft.Net.Http.Headers;
+using Microsoft.AspNet.Hosting;
 
 namespace ProductCompareDotNet.Controllers
 {
@@ -14,6 +19,7 @@ namespace ProductCompareDotNet.Controllers
         private readonly ProductCompareDbContext _db;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        //private IHostingEnvironment _environment;
 
         public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ProductCompareDbContext db)
         {
@@ -21,12 +27,36 @@ namespace ProductCompareDotNet.Controllers
             _signInManager = signInManager;
             _db = db;
         }
+
+        //public AccountController(IHostingEnvironment environment)
+        //{
+        //    _environment = environment;
+        //}
+
         public async Task<IActionResult> Index()
         {
             var user = await _userManager.FindByIdAsync(User.GetUserId());
 
             return View(user);
         }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Index(ICollection<IFormFile> files)
+        //{
+        //    var uploads = Path.Combine(_environment.WebRootPath, "uploads");
+        //    foreach (var file in files)
+        //    {
+        //        if (file.Length > 0)
+        //        {
+        //            var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+        //            await file.SaveAsAsync(Path.Combine(uploads, fileName));
+        //        }
+        //    }
+
+        //    var user = await _userManager.FindByIdAsync(User.GetUserId());
+
+        //    return View(user);
+        //}
 
         public IActionResult Register()
         {
@@ -76,6 +106,5 @@ namespace ProductCompareDotNet.Controllers
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index");
         }
-
     }
 }
